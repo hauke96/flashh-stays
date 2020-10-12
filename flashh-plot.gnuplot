c581f32d461515ba99d4c387c1df20f349f1d619
Hauke Stieler <mail@hauke-stieler.de>
2020-10-12 18:32:31 +0200

    Create own genuplot file per day and adjust rendering

--- flashh-plot.gnuplot
+++ flashh-plot.gnuplot
@@ -1,19 +1,27 @@
 set datafile separator ";"
 
+set title "FLASHH visitors on ${date}"
+
+# X dimension
 set xdata time
 set timefmt "%H:%M"
+set xrange ["08:30":"23:30"]
+set xtics "01:00"
+# Not necessary because raw data has already a nice time format
 # set format x "%H:%M"
 
+# Y dimension
 set yrange [0:150]
-set xrange ["08:30":"23:30"]
-
 set ytics 10
-set xtics "01:00"
 
-set key autotitle columnhead
+# Background grid
 set grid
+set style line 1 lc rgb '#aaaaaa' lt 0
+set grid xtics ytics ls 1
 
+# PNG rendering config
 set terminal png size 900,600 enhanced font 'Noto Sans,10'
-set output 'flashh-plot.png'
+set output 'flashh-stays_${date}.png'
 
-plot 'flashh-plot.csv' using 1:2 with lines
+# Start the plot
+plot 'flashh-stays_${date}.csv' using 1:2 with lines title "Amount of visitors"
--- flashh-stays.sh
+++ flashh-stays.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
 SLEEP=$((15 * 60))
-OUTPUT="flashh-stays_$(date "+%F").csv"
-OUTPUT_PNG="flashh-stays_$(date "+%F").png"
+DATE=$(date +%F)
+OUTPUT_CSV="flashh-stays_$DATE.csv"
+OUTPUT_GNUPLOT="flashh-stays_$DATE.gnuplot"
+OUTPUT_PNG="flashh-stays_$DATE.png"
 
-if [ ! -f $OUTPUT ]
+if [ ! -f $OUTPUT_CSV ]
 then
-	echo "Init new file $OUTPUT"
-	echo "date;stays" > $OUTPUT
+	echo "Init new file $OUTPUT_CSV"
+	echo "date;stays" > $OUTPUT_CSV
 fi
 
-DATE=$(date +%R)
+TIME=$(date +%R)
 STAYS=$(curl -s https://flashh.cashbloc.de/stays)
 
-echo "New entry: $DATE;$STAYS"
-echo "$DATE;$STAYS" >> $OUTPUT
+echo "New entry: $TIME;$STAYS"
+echo "$TIME;$STAYS" >> $OUTPUT_CSV
 
 # Render graphic
-cp $OUTPUT flashh-plot.csv
-gnuplot flashh-plot.gnuplot
-mv flashh-plot.png $OUTPUT_PNG
+#cp $OUTPUT_CSV flashh-plot.csv
+#gnuplot flashh-plot.gnuplot
+#mv flashh-plot.png $OUTPUT_PNG
+cp flashh-plot.gnuplot $OUTPUT_GNUPLOT
+sed -i "s/\${date}/$DATE/" $OUTPUT_GNUPLOT
+
+gnuplot $OUTPUT_GNUPLOT
+
 echo "PNG $OUTPUT_PNG rendered"
